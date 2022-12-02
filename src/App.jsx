951a088082768ecd6d582e0843ba29da4a188ce2
JK <jk.floods@gmail.com>
2022-12-02 03:04:52 +0100

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 import Settings from './pages/Settings'
 import Community from './pages/Community'
+import Header from './components/Header'
 
 function App() {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom'
 import Navbar from './Navbar'
+import logo from '../assets/logo.svg'
+import '../css/Header.css'
 
 function Header() {
   return (
     <header className="header">
-      <Link to="/">Logo</Link>
-      <Navbar />
+      <div className="content">
+        <Link to="/">
+          <img src={logo} alt="Kasa" className="logo" />
+        </Link>
+        <Navbar />
+      </div>
     </header>
   )
 }
