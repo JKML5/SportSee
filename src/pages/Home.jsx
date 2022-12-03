29d2abeb41d37870716ec1f4d36107dac35b0b4b
JK <jk.floods@gmail.com>
2022-12-03 14:08:29 +0100

    Home skeleton

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
+import '../css/Home.css'
+
 function Home() {
   return (
     <>
-      <h1>
+      <h1 className="title">
         Bonjour <strong>Thomas</strong>
       </h1>
       <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+      <div className="dashboard">
+        <div className="charts">
+          <div className="card">Stat</div>
+          <div className="card">Stat</div>
+          <div className="card">Stat</div>
+          <div className="card">Stat</div>
+        </div>
+        <div className="stats">
+          <div className="card">Stat</div>
+          <div className="card">Stat</div>
+          <div className="card">Stat</div>
+          <div className="card">Stat</div>
+        </div>
+      </div>
     </>
   )
 }
